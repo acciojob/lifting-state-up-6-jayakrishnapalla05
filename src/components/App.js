90c54dd332d99c006e5791101436df81b102d075
jayakrishnapalla05 <jayakrishnapalla05@gmail.com>
2023-08-25 17:14:06 +0530

    submitting code-Fri Aug 25 2023 17:14:04 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,56 @@
 
-import React from "react";
+import React, {useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [todos, settodos]=useState([
+    {id:1, title:"Learn React", status:false},
+    {id:2, title:"Build a React app", status:false},
+    {id:3, title:"Deploy the React app", status:false}
+  ]);
+
+  let id;
+  function handleClick(){
+    let task=[...todos];
+    task.forEach((e,i) =>{
+      if(e.id=== id)
+      {
+        task[i].status=true;
+      }
+    });
+    settodos(task);
+  }
+
+
+
   return (
     <div>
         {/* Do not remove the main div */}
+        <div>
+          <h1>Parent Component</h1>
+          <ul>
+            {
+              todos.map(e =>{
+                if(e.status=== true){
+                  return <li>{e.title}</li>
+                }
+              })
+            }
+          </ul>
+        </div>
+        <div>
+          <ul>
+            <h1>Child Component</h1>
+            {
+              todos.map((e)=>{
+                if(e.status === false){
+                  return <li>{e.title}<button onClick={()=>{ id=e.id; handleClick()}}>Complete</button></li>
+                }
+              })
+            }
+          </ul>
+        </div>
+
     </div>
   )
 }
